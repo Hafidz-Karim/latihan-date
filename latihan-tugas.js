a29214579d1a4f860b93979c7d20edaf17502de2
Hafidz <hafidzkarim18@gmail.com>
2024-09-18 10:33:32 +0700

    commit no: 2

--- latihan-tugas.js
+++ latihan-tugas.js
@@ -30,8 +30,32 @@ const monthName = months[date.getMonth()];
 console.log(monthName);
 // September
 
+
 // Menghitung waktu yang berbeda dalam milidetik
 const startDate = new Date('2024-01-01');
 const endDate = new Date('2024-12-31');
-const difference = endDate.______________ - startDate.______________; // Lengkapi untuk menghitung selisih dalam milidetik
-console.log(difference);
+
+// Menghitung selisih waktu dalam milidetik
+const differenceInMilliseconds = endDate.getTime() - startDate.getTime();
+
+// Menampilkan hasil ke konsol
+console.log("Selisih waktu dalam milidetik:", differenceInMilliseconds);
+//Output: 31536000000
+
+// Menampilkan hari dalam seminggu
+const tanggal = new Date('2024-09-18');
+const dayOfWeek = tanggal.getDay();
+console.log(dayOfWeek);
+// Output: 3
+
+
+// 6.Mendapatkan jam,menit,detik
+
+const currentTime = new Date();
+const hours = currentTime.getHours(); // Mendapatkan jam
+const minutes = currentTime.getMinutes(); // Mendapatkan menit
+const seconds = currentTime.getSeconds(); // Mendapatkan detik
+console.log(`Current time: ${hours}:${minutes}:${seconds}`);
+// Output: Current time: 10:28:50
+
+
